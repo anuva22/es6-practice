c01c26f6dac084ac4822599343ab2764ad40c17e
anuva22 <anuvaanuva22@gmail.com>
2021-02-15 13:11:52 +0600

    adding template

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = "Burger";
+const lastName = "Ology";
+//const fullName = firstName + " " + lastName + " my favorite";
+//const fullName2 = `${firstName} ${lastName} my favorite`;
+const fullName2 = `${firstName} ${40-2-10} my favorite`;
+console.log(fullName2);
+const multiLine = `I wanna go
+you wanna go?
+no, i dont wanna go
+okay, buy.`
+console.log(multiLine);
