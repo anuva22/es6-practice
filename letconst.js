91d92c87811be3b45730578db737468706c70759
anuva22 <anuvaanuva22@gmail.com>
2021-02-15 11:51:57 +0600

    adding 1st pactice

---
+++ letconst.js
@@ -0,0 +1,20 @@
+const hubby = "Burak Yourks";
+console.log(hubby);
+
+const numbers = [22 , 55];
+numbers[0] = 99;
+numbers.push(77);
+console.log(numbers);
+
+//const food = {name:"burger", code:"1234"};
+//console.log(food);
+let patientName = "boohoo";
+patientName = "doodoo";
+console.log(patientName);
+let sum = 0;
+//for(var i = 0; i < 10; i++){
+for(let i = 0; i < 10 ; i++){
+    sum = sum + i;
+    console.log(i);
+}
+    //console.log(i);
